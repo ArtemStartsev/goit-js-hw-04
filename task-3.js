f1f9eb11c8ef99a4a1b935c0d335fae131d1ae39
arr019 <artemstar19@gmail.com>
2025-04-10 21:24:16 +0300

    made all

---
+++ task-3.js
@@ -0,0 +1,8 @@
+function getElementWidth(content, padding, border) {
+    const borderBox = parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
+    console.log(`border-box size is ${borderBox}`);
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
